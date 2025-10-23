5830192ca2192312e24629283f1cde660e682f4a
tuananh1006 <tuananhhocai@gmail.com>
2025-10-23 11:49:24 +0700

    feat: init users route

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import express from 'express';
-import { envConfig } from './constants/config.js';
-import databaseService from './services/database.service.js';
+import express from "express";
+import { envConfig } from "./constants/config.js";
+import databaseService from "./services/database.service.js";
+import usersRouter from "./routes/users.routes.js";
 
 const app = express();
 const PORT = envConfig.port;
 databaseService.connect();
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(express.json());
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
